e2dee61a315c943c14ef40620bdbafd33cdff163
Yaroslav Dombrovskyi <dombrovskiy91@gmail.com>
2024-04-09 11:26:11 +0200

    fix bug
    fix bug

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
-import alias from 'vite-plugin-alias';
 
 export default defineConfig({
   plugins: [
     vue(),
-    alias({
-      entries: [
-        { find: '@', replacement: resolve(__dirname, 'src') },
-      ],
-    }),
   ],
 });
